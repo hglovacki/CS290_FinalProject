a27232a87f519e2b3870377ad27141bea8e4e436
hglovacki <harlin.glovacki@gmail.com>
2018-02-24 23:38:08 -0500

    carousel works

--- Website/Tests/test4.js
+++ Website/Tests/test4.js
@@ -1,33 +1,80 @@
 
-var stor = -1;
-var maxstor = 3;
-var timeout = 3500;
+/*
+ * this website: https://www.techrepublic.com/article/build-a-carousel-with-javascript/
+ * was helpful when formulating this script
+ */
 
-function autorot() {
-	showNext();
-	timeout = setTimeout('autorot();', timeout);
-}
+ var stor = -1;
+ var maxstor = 3;
+ var timeout = 2500;
 
-function rotateDiv(stor){
-  var divs = document.getElementById("storyContainer").getElementsByTagName("div");
-  for (var i=0; i < divs.length; i++ ) {
-    var div = divs[i];
-    if ( (div.id != "")) {
-	if(i != stor){
-        	div.style.display = "none";
-	}
-	else{
-		div.style.display = "block";
-	}
-    }
-  }
-}
+ function autorot() {
+   showNext();
+   setTimeout('autorot();', timeout);
+ }
 
-function showNext(){
-	if(stor < maxstor)
-		stor++;
-	else
-		stor=0;
 
-	rotateDiv(stor);
-}
+ function rotateDiv(stor){
+   var divs = document.getElementById("storyContainer").getElementsByTagName("div");
+   for (var i=0; i < divs.length; i++ ) {
+     var div = divs[i];
+     if ( (div.id != "")) {
+       	if(i != stor){
+               	div.style.display = "none";
+       	}
+       	else{
+       		div.style.display = "block";
+        }
+      }
+   }
+
+     var spans = document.getElementById("nav").getElementsByTagName("span");
+   for (var i=0; i < spans.length; i++ ) {
+     var span = spans[i];
+     if ( (span.id != "")) {
+       	if(i != stor)
+          span.className = "none";
+       	else
+       		span.className = "selStory";
+     }
+   }
+ }
+
+ function showNext(){
+   if(stor < maxstor)
+     stor++;
+   else
+     stor=0;
+
+   rotateDiv(stor);
+ }
+
+ function stoprot() {
+   clearTimeout(timeout);
+ }
+
+ function showPrev(){
+ 	if(stor > 0)
+ 		stor--;
+ 	else
+ 		stor=maxstor;
+
+ 	rotateDiv(stor);
+ }
+
+ function showStoryOne(){
+ 	stor=0;
+ 	rotateDiv(stor);
+ }
+ function showStoryTwo(){
+ 	stor=1;
+ 	rotateDiv(stor);
+ }
+ function showStoryThree(){
+ 	stor=2;
+ 	rotateDiv(stor);
+ }
+ function showStoryFour(){
+ 	stor=3;
+ 	rotateDiv(stor);
+ }
